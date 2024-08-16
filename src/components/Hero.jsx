cf9b7eb7044df951a85253b7f9f592324bcbb860
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-08-16 12:30:38 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Hero = () => {
   return (
-<div style={{ backgroundImage: "url('../assets/Hero.png')", height: "100vh", width: "100vw", backgroundSize: "cover", backgroundPosition: "center" }}>
-  Test Background
+<div style={{ backgroundImage: "url('https://via.placeholder.com/150')" }}
+ ></div> Test Background
 </div>
   );
 };

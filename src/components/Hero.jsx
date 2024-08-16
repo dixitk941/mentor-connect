53132f30a947973003144de81055ff02f6bb4213
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-08-16 14:11:24 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 const Hero = () => {
   return (
     <div 
-      className="relative bg-cover bg-center h-screen text-white flex items-center justify-center" 
-      style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/mentor-connect-74cc5.appspot.com/o/Hero.png?alt=media&token=aa24b32b-338a-4dc4-8eec-45a00be7fc77')" }}
+      className="relative bg-cover bg-center h-screen text-white flex items-center justify-center"
+      style={{
+        backgroundImage: `
+          url('https://firebasestorage.googleapis.com/v0/b/mentor-connect-74cc5.appspot.com/o/Hero.png?alt=media&token=aa24b32b-338a-4dc4-8eec-45a00be7fc77')`, // Desktop
+
+        '@media (max-width: 1024px)': {
+          backgroundImage: `
+          url('https://firebasestorage.googleapis.com/v0/b/mentor-connect-74cc5.appspot.com/o/Hero(tablet).png?alt=media&token=41571eb1-d6c3-42c5-9b3b-13f5f24aa946')`, // Tablet
+        },
+
+        '@media (max-width: 768px)': {
+          backgroundImage: `
+          url('https://firebasestorage.googleapis.com/v0/b/mentor-connect-74cc5.appspot.com/o/Hero(mobile).png?alt=media&token=dcd23918-bb21-4b2c-9d18-e055a2947006')`, // Mobile
+        }
+      }}
     >
       <div className="relative z-10 text-center">
         <h1 className="text-6xl font-extrabold mb-4 animate-fade-in">Welcome to Mentor Connect</h1>
